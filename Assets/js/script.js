564f48814c5c997907eca038547710daebb9db32
Rohan <mbakibillahrohan@gmail.com>
2022-10-17 22:29:35 +0600

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+
+// here are all the function 
+
+
+
+
+// here started the code 
+
+let tab_buttons = document.getElementsByClassName('tab-btn');
+tab_buttons[0].addEventListener("click", ()=>{
+    console.log("clicked")
+})
